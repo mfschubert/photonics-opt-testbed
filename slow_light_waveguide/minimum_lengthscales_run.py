7b7e99a877eb8014ac4ffe62ee0ecb54694fd0f8
Martin Schubert <mfschubert@gmail.com>
2024-04-01 17:30:59 -0700

    Fix binarization

--- slow_light_waveguide/minimum_lengthscales_run.py
+++ slow_light_waveguide/minimum_lengthscales_run.py
@@ -7,11 +7,10 @@ files = os.listdir(path)
 files.sort()
 print('Design file, solid minimum lengthscale (a), void minimum lengthscale (a), minimum lengthscale (a)')
 
-design_size = (1, 10.2)
-
 for file_name in ['Design_Dnum_2.csv', 'HigRes_DesMatch_Opt_Dnum_2.csv']:  
-    design_pattern = np.loadtxt(path+file_name, delimiter=',')
+    design_permittivity = np.loadtxt(path+file_name, delimiter=',')
 
-    binary_design_pattern = design_pattern > 0.5
+    midpoint = (np.amax(design_permittivity) + np.amin(design_permittivity)) / 2
+    binary_design_pattern = design_permittivity > midpoint
     solid_mls, void_mls = imageruler.minimum_length_scale(binary_design_pattern, periodic=(True, True))
     print(file_name, solid_mls, void_mls, min(solid_mls, void_mls))
